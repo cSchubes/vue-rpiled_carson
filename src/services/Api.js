c2f78f42ab9f44f8a56256870c71984602d353e6
Carson Schubert <carson.schubert14@gmail.com>
2025-09-27 02:39:47 -0700

    Update Api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export default() => {
   return axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://2c6c338e092dbe1c0433d3d4fdae1a28.balena-devices.com/api' : 'http://localhost:8081/api',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://4fe20dd2ef64963a61765e319332ab35.balena-devices.com/api' : 'http://localhost:8081/api',
     headers: {'Access-Control-Allow-Origin': '*'}
   })
 }
